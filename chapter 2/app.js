7601836619a61cc404fa4498129955ba4e055833
yumna1623 <yumnanasir1623@gmail.com>
2024-10-09 20:57:54 +0500

    chapter 1-2

---
+++ chapter 2/app.js
@@ -0,0 +1,29 @@
+// -----------------------------------------    question 1--------------------------
+// let username;
+// -----------------------------------------    question 2--------------------------
+// let myame="yumna nasir"
+// -----------------------------------------    question 3--------------------------
+// let message="hello world"
+// alert(message)
+// -----------------------------------------    question 4--------------------------
+// const name="yumna"
+// alert(name)
+// const age="19"
+// alert(age)
+// const post="certified mobile application developer"
+// alert(post)
+// -----------------------------------------    question 5--------------------------
+// let pizza="PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza)
+// -----------------------------------------    question 6--------------------------
+// let email="yumnanasir1623@gmail.com"
+// let message="my email address is"
+// alert(email+message)
+// -----------------------------------------    question 7--------------------------
+// let book="i am trying to learn from book A smarter way to learn from book "
+// alert(book)
+// -----------------------------------------    question 8--------------------------
+// document.write("yeah i can write html content through javascript")
+
+// -----------------------------------------    question 9--------------------------
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
