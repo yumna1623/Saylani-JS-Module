91770ed56b2c8703149c8d977d8aae416fe4afea
yumna1623 <yumnanasir1623@gmail.com>
2024-10-11 15:16:53 +0500

    till  chapter 11

---
+++ chapter 9-11/app.js
@@ -0,0 +1,68 @@
+// let city=prompt("enter city name")
+// city =city.toLowerCase()
+// if( city=="karachi")
+// alert("Welcome to city of lights")
+// ----------------------------------------------
+
+// let gender=prompt("enter your gender")
+// if(gender=="male"){
+//     alert("Good Morning Sir")
+// }
+// else{
+//     alert("Good Morning Mam")
+// }
+// ------------------------------------------------
+
+// let fuel = prompt("Enter the remaining fuel in your car (in litres):");
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// } else {
+//     alert("You have enough fuel");
+// }
+// ---------------------------------------------------------
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// true
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// false
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// 2vtrue
+
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// //
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+// //
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
