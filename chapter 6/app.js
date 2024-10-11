19310d09c655210ce922eb105f1b5007cd58211f
yumna1623 <yumnanasir1623@gmail.com>
2024-10-11 13:58:31 +0500

    till  chapter 6

---
+++ chapter 6/app.js
@@ -0,0 +1,46 @@
+// -------------------------------------QUESTION 1-------------------------------------------
+
+// let a=10
+// document.write("Result <br>")
+// document.write("The Value of a is "+ a+"<br>")
+// document.write("---------------------"+"<br>")
+
+// document.write("The Value of ++a is "+ ++a+"<br>")
+// document.write("Now value of a is "+a+"<br>")
+// document.write("----------------o-----"+"<br>")
+
+
+// document.write("The Value of a++ is "+ a++ +"<br>")
+// document.write("Now value of a is "+a+"<br>")
+// document.write("---------------------"+"<br>")
+
+// document.write("The Value of --a is "+ --a+"<br>")
+// document.write("Now value of a is "+a+"<br>")
+// document.write("---------------------"+"<br>")
+
+// document.write("The Value of a-- is "+ a-- +"<br>")
+// document.write("Now value of a is "+a+"<br>")
+// document.write("---------------------"+"<br>")
+
+// -------------------------------------QUESTION 2-------------------------------------------
+// var a=2,b=1
+// --a - --b → 1 - 0 = 1
+// --a - --b + ++b → 1 + 1 = 2
+// --a - --b + ++b + b-- → 2 + 1 = 3
+
+// -------------------------------------QUESTION 3-------------------------------------------
+// input=prompt("enter your name")
+// alert("welcome "+input)
+
+// -------------------------------------QUESTION 6-------------------------------------------
+// let subject1 = prompt("Enter 1st subject name") 
+// let subject2 = prompt("Enter 2nd subject name") 
+// let subject3 = prompt("Enter 3rd subject name") 
+// let total_marks=100
+// let marks1 = prompt("Enter 1st subject marks") 
+// let marks2 = prompt("Enter 2nd subject marks") 
+// let marks3 = prompt("Enter 3rd subject marks") 
+
+// let total=marks1+marks2+marks3
+// let result=total/total_marks
+
